41985a4b14376855fe63ca7e397977828259f19b
jakedevar <devarennesjacob@gmail.com>
2022-09-13 19:44:52 -0500

    commit two

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-// const bodyParser = require('body-parser');
-// const querystring = require('querystring');
+const Pool = require('pg').Pool
+const pool = new Pool()
 
 const app = express()
 const PORT = 3002
+/*
+A: take the binid and path, send back with res.send (to display url and id 
+and save to local storage) postgres save to 
+*/
+const createRandomStr = () => {
+	let str = ""
+	const alpha = "abcdefghijklmnopqrstuvwxyz1234567890"
+	for (i = 0; i < 10; i++) {
+		const ind = Math.floor(Math.random() * i)
+		str += alpha[ind]
+	}
+	return str
+}
+// mongoose schema 
+// create bin and targetUrl and 
+app.post('/create', (req, res) => {
+	const binKey = createRandomStr()
+	const path = 'http\://localhost\:3002/' + binKey
+	pool.query(`INSERT INTO bin VALUES(DEFAULT, '${binKey}');`)
+	res.send([binKey, path])
+})
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+// take binId from request string and save request to the database
+app.all('/:binKey', (req, res) => {
+	// take request string and parse id get from db 
+	// local storage
+})
+
 app.post('/api/bin', (req, res) => {
 	const data = []
 	for (prop in req) {
@@ -18,46 +44,9 @@ app.post('/api/bin', (req, res) => {
 
 app.get('/api/bin', (req, res) => {
 	const data = [req.rawHeaders, req.query, req.params]
-	res.send(data)
+	res.send(createRandomStr())
 })
 
 app.listen(process.env.PORT || PORT, () => {
 	console.log('listening on port 3002')
 })
-// Host: dnsdatacheck.2ruehnqius4x8kd0.b.requestbin.net
-//
-// Connection: close
-//
-// User-Agent: GitHub-Hookshot/3116550
-//
-// Accept: */*
-//
-// X-Github-Delivery: 84783b4e-32da-11ed-98da-a6627229a26f
-//
-// X-Github-Event: push
-//
-// X-Github-Hook-Id: 379030870
-//
-// X-Github-Hook-Installation-Target-Id: 459729154
-//
-// X-Github-Hook-Installation-Target-Type: repository
-//
-// Content-Type: application/x-www-form-urlencoded
-//
-// X-Request-Id: 5b807b3e-844e-4f10-b2ac-8ed4081057c9
-//
-// X-Forwarded-For: 140.82.115.92
-//
-// X-Forwarded-Proto: http
-//
-// X-Forwarded-Port: 80
-//
-// Via: 1.1 vegur
-//
-// Connect-Time: 0
-//
-// X-Request-Start: 1663014963469
-//
-// Total-Route-Time: 0
-//
-// Content-Length: 10080
